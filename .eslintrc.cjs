08507f60383ca4d5856e5e3f657a8f4cb4e251e3
Dea9703 <dea9703@163.com>
2023-04-27 22:43:30 +0800

    修改ecmaVersion版本，添加eslint rules校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,16 +12,17 @@ module.exports = {
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
     parser: '@typescript-eslint/parser',
     tsconfigRootDir: __dirname,
     project: ['tsconfig.json'],
-    extraFileExtensions: ['.vue'],
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    extraFileExtensions: ['.vue']
   },
   plugins: [
     'vue'
   ],
   rules: {
+    'vue/multi-word-component-names': 'off'
   }
 }
